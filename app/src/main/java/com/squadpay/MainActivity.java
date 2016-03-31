9262a9fec921d37734d5b5f8e8b146a29a3b1f72
NotYourLoginID <cdsekira@msn.com>
2016-03-31 15:32:12 -0400

    Added SquadPay object

--- app/src/main/java/com/squadpay/MainActivity.java
+++ app/src/main/java/com/squadpay/MainActivity.java
@@ -3,6 +3,8 @@ package com.squadpay;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 // usually extends AppCompatActivity but I changed it to activity so that there would
 // be no action bar. May be a better way to fix this
 public class MainActivity extends Activity{
@@ -11,5 +13,8 @@ public class MainActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Firebase.setAndroidContext(this);
+
+        Firebase myFirebaseRef = new Firebase("https://squadpay-live.firebaseio.com/");
     }
 }
