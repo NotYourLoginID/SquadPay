d77b859188b8bf2e2a0d19305c868bf3343cd803
NotYourLoginID <cdsekira@msn.com>
2016-04-19 03:03:38 -0400

    Enabled Firebase email login

--- app/src/main/java/com/squadpay/MainActivity.java
+++ app/src/main/java/com/squadpay/MainActivity.java
@@ -1,33 +1,88 @@
 package com.squadpay;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 // usually extends AppCompatActivity but I changed it to activity so that there would
 // be no action bar. May be a better way to fix this
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     private static final String FIREBASE_URL = "https://squadpay-live.firebaseio.com";
-    private Firebase mFirebaseRef;
+    private Firebase squadPayFBRef;
+
+    private EditText loginEmailField;
+    private EditText loginPasswordField;
+    private CoordinatorLayout mainCoordinatorLayout;
+    private Button loginButton;
+    private Button signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // This is a test to see if commit and push works for me (Max B.)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Firebase.setAndroidContext(this);
 
-        mFirebaseRef = new Firebase(FIREBASE_URL);
-    }
+        squadPayFBRef = new Firebase(FIREBASE_URL);
 
-    public void attemptLogin(View view){
-        Intent intent = new Intent(this, SquadPayTabs.class);
-        startActivity(intent);
+        loginEmailField = (EditText) findViewById(R.id.loginEmail);
+        loginPasswordField = (EditText) findViewById(R.id.loginPassword);
+        mainCoordinatorLayout = (CoordinatorLayout) findViewById(R.id.mainCoordinatorLayout);
+        loginButton = (Button) findViewById(R.id.loginButton);
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager kbm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                // this line below apparently will throw exceptions, the comment tells android to shutup about it for now
+                //noinspection ConstantConditions
+                kbm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+                squadPayFBRef.authWithPassword(loginEmailField.getText().toString(), loginPasswordField.getText().toString(), new Firebase.AuthResultHandler() {
+                    Snackbar loginSnackbar = Snackbar.make(mainCoordinatorLayout, "Invalid Login", Snackbar.LENGTH_SHORT);
+
+                    @Override
+                    public void onAuthenticated(AuthData authData) {
+                        loginSnackbar.setText("Login Successful");
+                        loginSnackbar.setCallback(new Snackbar.Callback() {
+                            @Override
+                            public void onDismissed(Snackbar s, int event) {
+                                startActivity(new Intent(MainActivity.this, SquadPayTabs.class));
+                                MainActivity.this.finish();
+                            }
+                        });
+                        loginSnackbar.show();
+                    }
+
+                    @Override
+                    public void onAuthenticationError(FirebaseError firebaseError) {
+                        loginSnackbar.setCallback(new Snackbar.Callback() {
+                            @Override
+                            public void onDismissed(Snackbar s, int event) {
+                                loginEmailField.setText("");
+                                loginPasswordField.setText("");
+                            }
+                        });
+                        loginSnackbar.show();
+                    }
+                });
+            }
+        });
     }
 }
+
 
