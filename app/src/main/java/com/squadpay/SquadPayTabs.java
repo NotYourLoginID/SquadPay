42420b24a2b28fb909c7952c0590f07459d22edf
Max Brinegar <mxbrinegar@gmail.com>
2016-04-26 18:12:36 -0400

    The Code for this branch is not complete and needs to be organized. But it runs well so far. This branch shows recycler views for our tabs.

--- app/src/main/java/com/squadpay/SquadPayTabs.java
+++ app/src/main/java/com/squadpay/SquadPayTabs.java
@@ -3,10 +3,13 @@ package com.squadpay;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -20,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -61,7 +65,6 @@ public class SquadPayTabs extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-
 
         // Chris - removed the FAB and SnackBar
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -117,6 +120,9 @@ public class SquadPayTabs extends AppCompatActivity {
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private RecyclerView mRecyclerView;
+        private TabDataAdapter mAdapter;
+        private LinearLayoutManager mLayoutManager;
 
         public PlaceholderFragment() {
         }
@@ -135,10 +141,40 @@ public class SquadPayTabs extends AppCompatActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_squad_pay_tabs, container, false);
+            /*View rootView = inflater.inflate(R.layout.fragment_squad_pay_tabs, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));*/
+            View view = inflater.inflate(R.layout.fragment_squad_pay_tabs, container, false);
+            mRecyclerView = (RecyclerView) view.findViewById(R.id.tabs_recycler_view);
+
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            mRecyclerView.setHasFixedSize(true);
+
+            // use a linear layout manager
+            mLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+            mRecyclerView.setLayoutManager(mLayoutManager);
+
+            String[] data = new String[100];
+
+            if (getArguments().getInt(ARG_SECTION_NUMBER) == 3) {
+                for (int i = 0; i < 100; i++) {
+                    data[i] = "Item name" + "\n" + "Squad involved" + "\n" + "Paid status" + "\n" + "$Amount";
+                }
+
+            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
+                for (int i = 0; i < 100; i++) {
+                    data[i] = "Name of Squad" + "\n" + "Number of members" + "\n" + "Creation date";
+                }
+            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 1){
+                for (int i = 0; i < 100; i++) {
+                    data[i] = "Brief recent activity";
+                }
+            }
+            // specify an adapter
+            mAdapter = new TabDataAdapter(data);
+            mRecyclerView.setAdapter(mAdapter);
+            return view;
         }
     }
 
@@ -147,6 +183,8 @@ public class SquadPayTabs extends AppCompatActivity {
      * one of the sections/tabs/pages.
      */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
+        final private int PAGE_COUNT = 3;
+        final private String tabTitles[] = new String[] {"FEED", "SQUADS", "EXPENSES"};
 
         public SectionsPagerAdapter(FragmentManager fm) {
             super(fm);
@@ -160,22 +198,60 @@ public class SquadPayTabs extends AppCompatActivity {
         }
 
         @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
+        public int getCount() { return PAGE_COUNT; }
 
         @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Feed";
-                case 1:
-                    return "Squads";
-                case 2:
-                    return "Expenses";
-            }
-            return null;
+        public CharSequence getPageTitle(int position) { return tabTitles[position]; }
+    }
+}
+class TabDataAdapter extends RecyclerView.Adapter<TabDataAdapter.ViewHolder> {
+    private String[] mDataset;
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public FrameLayout mFrameLayout;
+        public ViewHolder(FrameLayout v) {
+            super(v);
+            // still need to figure out this part below before I can add other views
+            // to this recycler view. This might not be necessary right now
+            if (v.getParent() != null) { ((ViewGroup) v.getParent()).removeView(v); }
+            mFrameLayout = v;
+            mTextView = (TextView) v.findViewById(R.id.textViewTabs);
+            mTextView.setTextSize(16f);
         }
+    }
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public TabDataAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public TabDataAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                           int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recyclerview_tabs, parent, false);
+        // set the view's size, margins, paddings and layout parameters here
+        // ...
+        ViewHolder vh = new ViewHolder((FrameLayout) v.findViewById(R.id.frameLayoutTabs));
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
     }
 }
